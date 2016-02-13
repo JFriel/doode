b44517746c4bea3d4787c089161769a8b5641e20
andrebododea <andrebododea@sbcglobal.net>
2016-02-13 22:50:42 +0000

    Sends SMS when run to all the phone numbers in the web/static/edinburgh_numbers.txt

--- SMS_sender.py
+++ SMS_sender.py
@@ -1,9 +1,5 @@
-import schedule 
-import time
-
 # Download the twilio-python library from http://twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
-
 
 '''
 ****************************************************
@@ -14,22 +10,24 @@ from twilio.rest import TwilioRestClient
 ****************************************************
 '''
 
-# phone_number_array = ["+447584429236","+447935456467"]
-phone_number_array = []
-
-for line in open("phone_numbers.txt", 'r'):
-	if(line[0] == '4' and line[1] == '4' and line[12] == '\n'):
+# Parsing the user input into valid phone numbers. Valid format is as follows: "+447584429236". 
+# Should be properly parsed from "07584429236" or "447584429236" or "+447584429236" everything else is invalid syntax
+phone_number_array = []     
+for line in open("web/static/edinburgh_numbers.txt", 'r'):
+	if(line[0:2] == '44' and (line[12] == '\n' or line[12] == ' ')):
 		array = '+' + line[0:12]
 		phone_number_array.append(array)
-	else:
-		print("Error: phone number not entered in proper format.")
+	elif(line[0:3] == '+44' and (line[13] == '\n' or line[13] == ' ')):
+		array = line[0:13]
+		phone_number_array.append(array)
+	elif(line[0:2] == '07' and (line[11] == '\n' or line[11] == ' ')):
+		array = '+44' + line[1:11]
+		phone_number_array.append(array)
 
 print phone_number_array
 
-# Andre's Phone Number: +447584429236
-# Dan's Phone number: +447935456467
-
 def sendtext():
+
 	# Find these values at https://twilio.com/user/account
 	account_sid = "AC0035cee82225e4f04cf4a922e9412e55"
 	auth_token = "dc537119815f8131bf6314416ffb196f"
@@ -41,10 +39,5 @@ def sendtext():
 		message = client.messages.create(to=str(phoneNum), from_="+441133207976", body="Surf's up brah!")
 
 
-# Sets scheduler to place a pending at 9:00 every day
-schedule.every().day.at("21:00").do(sendtext)
-
-# Loops forever, running the pending messages. 
-while True:
-	schedule.run_pending()
-	time.sleep(1)
+if __name__ == "__main__":
+    sendtext();
