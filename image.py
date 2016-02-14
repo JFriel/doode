c191281c400d86e60796b463886084e6ec3c0484
JFriel <james.friel4@gmail.com>
2016-02-14 10:59:27 +0000

    image.py is the final code, just run that to start the wave anaysis

--- image.py
+++ image.py
@@ -8,29 +8,43 @@ from scipy.cluster.hierarchy import cophenet
 from scipy.spatial.distance import pdist
 import os
 import imghdr
+from twilio.rest import TwilioRestClient
+import time
+phone_number_array = []
+vals =[]
 
-
-
-
+for line in open("web/static/edinburgh_numbers.txt", 'r'):
+	if(line[0:2] == '44' and (line[12] == '\n' or line[12] == ' ')):
+		array = '+' + line[0:12]
+		phone_number_array.append(array)
+	elif(line[0:3] == '+44' and (line[13] == '\n' or line[13] == ' ')):
+		array = line[0:13]
+		phone_number_array.append(array)
+	elif(line[0:2] == '07' and (line[11] == '\n' or line[11] == ' ')):
+		array = '+44' + line[1:11]
+		phone_number_array.append(array)
 
 def check_file():
 
-     
+
     path = 'images_coldingham/'
     listing = os.listdir(path)
     for infile in listing:
         print infile
+        #name waterline breaklength wavelength
+        vals = infile.split()
+
         with open("data.txt", "wb") as f :
         	#if(imghdr.what(infile) == 'jpg'):
         	f.write(look_image(infile))
-		
+
 
 
 def look_image(name):
 	print name
 	img = cv2.imread('images_coldingham/'+name)
 	imgThresh = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	imgThresh = cv2.erode(imgThresh, np.ones((5,5),np.uint8))         
+	imgThresh = cv2.erode(imgThresh, np.ones((5,5),np.uint8))
 
 	points = np.zeros((70,1))
 
@@ -43,16 +57,16 @@ def look_image(name):
 			pt1 = (-100000,line[1])
 			pt2 = (100000,line[3])
 			#cv2.line(img, pt1, pt2, (0,0,255), 3)
-			points[thresh_num] = line[1]	
+			points[thresh_num] = line[1]
 
 
 		thresh_num = thresh_num + 1
 
 
 	centroid, label = scipy.cluster.vq.kmeans2(points,20)
-				
 
-	tally = [0]*20	
+
+	tally = [0]*20
 
 	for x in label:
 		tally[x]= tally[x] + 1
@@ -64,14 +78,14 @@ def look_image(name):
 			pt1 = (-100000,int(point))
 			pt2 = (100000,int(point))
 			cv2.line(img, pt1, pt2, (19,201,253), 3)
-		
+
 		if point > 550:
 			centroid = np.delete(centroid, i)
 		i = i + 1
-		
-	
+
+
 	largest = heapq.nlargest(3,centroid)
-	
+
 	water_line_reference = largest[0]
 	break_length_paralax = (largest[0] - largest[1])
 	wave_length_paralax = (largest[1] - largest[2])
@@ -81,10 +95,34 @@ def look_image(name):
 
 	return name+"	"+str(water_line_reference)+"	"+str(break_length_paralax)+"	"+str(wave_length_paralax)
 
-	#cv2.imshow("frame",img)	
+	#cv2.imshow("frame",img)
 	#cv2.waitKey(0)
 	#cv2.destroyAllWindows()
+def gnarometer(wavelength,period,frequency):
+    return wavelength * (period*2) + (frequency*1.5)
 
-		
-        
-	
+def sendtext():
+
+	# Find these values at https://twilio.com/user/account
+	account_sid = "AC0035cee82225e4f04cf4a922e9412e55"
+	auth_token = "dc537119815f8131bf6314416ffb196f"
+	client = TwilioRestClient(account_sid, auth_token)
+
+	for phoneNum in phone_number_array:
+		# Twilio Number: +441133207976
+		print phoneNum
+		message = client.messages.create(to=str(phoneNum), from_="+441133207976", body="Surf's up brah!")
+
+while True:
+    vals=[]
+    checkfile()
+    gn = gnarometer(vals[1],vals[2],vals[3])
+    if (gn >= 50):
+        sendtext()
+        time.sleep(10800)
+    else:
+        time.sleep(1200)
+
+
+
+
